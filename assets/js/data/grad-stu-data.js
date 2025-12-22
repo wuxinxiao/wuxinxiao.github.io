7496a1477fdbdf80207cf450b52c8d2847c2b1f8
Byter-s <yiqi.song.02@gmail.com>
2025-12-22 11:40:50 +0800

    Refactor code structure to separate data from rendering

---
+++ assets/js/data/grad-stu-data.js
@@ -0,0 +1,151 @@
+// Alumni Data
+const gradData = {
+    "phd": [
+        {
+            "name": "田孟潇",
+            "destination": "北京农业科学院 师资博士后",
+            "nameEN": "Tian Mengxiao",
+            "destinationEN": "Beijing Research Center of Intelligent Equipment for Agriculture PostPhD studental Teaching Fellow"
+        },
+        {
+            "name": "杨硕",
+            "destination": "深圳北理莫斯科大学 副教授",
+            "nameEN": "Yang Shuo",
+            "destinationEN": "Shenzhen MSU-BIT University Associate Professor"
+        },
+        {
+            "name": "陈谨",
+            "destination": "航天智能院 研发工程师",
+            "nameEN": "Chen Jin",
+            "destinationEN": "Aerospace Intelligence Research Institute R&D Engineer"
+        },
+        {
+            "name": "赵文天",
+            "destination": "北京理工大学 博士后",
+            "nameEN": "Zhao Wentian",
+            "destinationEN": "Beijing Institute of Technology PostPhD"
+        },
+        {
+            "name": "王晗",
+            "destination": "北京林业大学 副教授",
+            "nameEN": "Wang Han",
+            "destinationEN": "Beijing Forestry University Associate Professor"
+        },
+        {
+            "name": "宋浩",
+            "destination": "腾讯 研究员",
+            "nameEN": "Song Hao",
+            "destinationEN": "Tencent Researcher"
+        },
+        {
+            "name": "侯静怡",
+            "destination": "北京科技大学 师资博士后",
+            "nameEN": "Hou Jingyi",
+            "destinationEN": "Beijing University of Science and Technology PostPhD studental Teaching Fellow"
+        }
+    ],
+    "master": [
+        {
+            "name": "李鸿熙",
+            "destination": "美图影像研究院 CV算法工程师",
+            "nameEN": "Li Hongxi",
+            "destinationEN": "Meitu Imaging and Vision Lab CV Algorithm Engineer"
+        },
+        {
+            "name": "黄希晴",
+            "destination": "国家电网大数据 研发工程师",
+            "nameEN": "Huang Xiqing",
+            "destinationEN": "STATE GRID Corporation of China Big Data Engineer"
+        },
+        {
+            "name": "朱宇博",
+            "destination": "中国科学院空天信息创新研究院 助理工程师",
+            "nameEN": "Zhu Yubo",
+            "destinationEN": "Aerospace Information Research Institute Assistant engineer"
+        },
+        {
+            "name": "石语珩",
+            "destination": "中国银行总行 信科管培生",
+            "nameEN": "Shi Yuheng",
+            "destinationEN": "Bank of China Information management Pearson"
+        },
+        {
+            "name": "闻子涵",
+            "destination": "中国空间技术研究院 算法工程师",
+            "nameEN": "Wen Zihan",
+            "destinationEN": "China Academy of Space Technology Algorithm Engineer"
+        },
+        {
+            "name": "纪校锋",
+            "destination": "字节跳动 算法工程师",
+            "nameEN": "Ji Xiaofeng",
+            "destinationEN": "ByteDance Algorithm Engineer"
+        },
+        {
+            "name": "伊嘉诚",
+            "destination": "中国人民解放军战略支援部队 助理工程师",
+            "nameEN": "Yi Jiacheng",
+            "destinationEN": "The PLA Strategic Support Force Assistant engineer"
+        },
+        {
+            "name": "李彤",
+            "destination": "阿里巴巴 算法工程师",
+            "nameEN": "Li Tong",
+            "destinationEN": "Alibaba Algorithm Engineer"
+        },
+        {
+            "name": "林瀚熙",
+            "destination": "字节跳动 算法工程师",
+            "nameEN": "Lin Hanxi",
+            "destinationEN": "ByteDance Algorithm Engineer"
+        },
+        {
+            "name": "刘超",
+            "destination": "阿里巴巴 高级算法工程师",
+            "nameEN": "Liu Chao",
+            "destinationEN": "Alibaba Senior Algorithm Engineer"
+        },
+        {
+            "name": "余非梧",
+            "destination": "阿里达摩院 开发工程师",
+            "nameEN": "Yu Feiwu",
+            "destinationEN": "Alibaba DAMO Development Engineer"
+        },
+        {
+            "name": "朱冰",
+            "destination": "阿里云 数据工程师",
+            "nameEN": "Zhu Bing",
+            "destinationEN": "Alibaba Cloud Data Engineer"
+        },
+        {
+            "name": "孙宇超",
+            "destination": "旷视科技 算法研究员",
+            "nameEN": "Sun Yuchao",
+            "destinationEN": "Beijing Megvii Co., Ltd Algorithm Researcher"
+        },
+        {
+            "name": "王瑞琦",
+            "destination": "小米 产品经理",
+            "nameEN": "Wang Ruiqi",
+            "destinationEN": "MI Product Manager"
+        },
+        {
+            "name": "滑蕊",
+            "destination": "航空工业制造院 信息化管理",
+            "nameEN": "Hua Rui",
+            "destinationEN": "AVIC Manufacturing Technology Institute Information Management"
+        },
+        {
+            "name": "陈佳露",
+            "destination": "小米 算法工程师",
+            "nameEN": "Chen Jialu",
+            "destinationEN": "MI Algorithm Engineer"
+        },
+        {
+            "name": "李天宇",
+            "destination": "京投 管培生",
+            "nameEN": "Li Tianyu",
+            "destinationEN": "Beijing Infrastructure Investment Management Trainee"
+        }
+    ]
+};
---
+++ assets/js/data/stu-data.js
@@ -0,0 +1,67 @@
+// Current Students Data
+const stuData = [
+    {
+        "name": "齐雅昀",
+        "degree": "博士生",
+        "area": "video caption",
+        "img": "assets/images/group/qiyayun.jpg",
+        "nameEN": "Qi Yayun",
+        "degreeEN": "phD student"
+    },
+    {
+        "name": "陈军",
+        "degree": "博士生",
+        "area": "incremental learning",
+        "img": "assets/images/group/chenjun.jpg",
+        "nameEN": "Chen Jun",
+        "degreeEN": "phD student"
+    },
+    {
+        "name": "尚子睿",
+        "degree": "博士生",
+        "area": "video grounding",
+        "img": "assets/images/group/shangzirui.jpg",
+        "nameEN": "Shang Zirui",
+        "degreeEN": "phD student"
+    },
+    {
+        "name": "宋奕圻",
+        "degree": "博士生",
+        "area": "multimodal reasoning",
+        "img": "assets/images/group/songyiqi.jpg",
+        "nameEN": "Song Yiqi",
+        "degreeEN": "phD student"
+    },
+    {
+        "name": "王泳棋",
+        "degree": "硕士生",
+        "area": "video relation detection",
+        "img": "assets/images/group/wangyongqi.jpg",
+        "nameEN": "Wang Yongqi",
+        "degreeEN": "MS student"
+    },
+    {
+        "name": "王子奕",
+        "degree": "硕士生",
+        "area": "domain generalization",
+        "img": "assets/images/group/wangziyi.jpg",
+        "nameEN": "Wang Ziyi",
+        "degreeEN": "MS student"
+    },
+    {
+        "name": "朱荣江",
+        "degree": "硕士生",
+        "area": "video action recognition",
+        "img": "assets/images/group/zhurongjiang.jpg",
+        "nameEN": "Zhu Rongjiang",
+        "degreeEN": "MS student"
+    },
+    {
+        "name": "谭云腾",
+        "degree": "硕士生",
+        "area": "LLM-based agents",
+        "img": "assets/images/group/tanyunteng.jpg",
+        "nameEN": "Tan Yunteng",
+        "degreeEN": "MS student"
+    }
+];
