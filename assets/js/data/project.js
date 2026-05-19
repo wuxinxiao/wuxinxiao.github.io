00bc140affb6181d6952268ef63eab8089d0b062
Byter-s <yiqi.song.02@gmail.com>
2026-05-19 15:15:35 +0800

    add Project card

---
+++ assets/js/data/project.js
@@ -0,0 +1,18 @@
+const projectData = [
+    {
+        name: "视频理解领域泛化",
+        nameEn: "Video Understanding Domain Generalization",
+        description: "VUDG: A Dataset for Video Understanding Domain Generalization",
+        descriptionEn: "VUDG: A Dataset for Video Understanding Domain Generalization",
+        bg: "assets/images/project/vudg.png",
+        url: "https://vudg-video.github.io"
+    },
+    {
+        name: "开放词汇视频理解",
+        nameEn: "Open-Vocabulary Video Understanding",
+        description: "Open-Vocabulary Video Understanding｜Visual Relationship Detection",
+        descriptionEn: "Open-Vocabulary Video Visual Relationship Detection",
+        bg: "assets/images/project/ov.png",
+        url: "https://wangyongqi558.github.io"
+    }
+];
